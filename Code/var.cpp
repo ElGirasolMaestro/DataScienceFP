f14cdc08c94c8d0bd1db40b33e701b70b5a00577
ElGirasolMaestro <63981220+ElGirasolMaestro@users.noreply.github.com>
2023-06-26 20:14:24 -0500

    archivos

---
+++ Code/var.cpp
@@ -0,0 +1,17 @@
+El conjunto de datos contiene información de 1000 usuarios con diferentes características, incluyendo su origen y si compraron o no una bicicleta.Los campos incluidos son :
+
+ID: Identificación única de los compradores.
+Estado Civil : Indica si el comprador está casado o soltero.
+Género : El género del comprador, ya sea hombre o mujer.
+Ingresos : Los ingresos del comprador en un período determinado.
+Hijos : El número de hijos que tiene el comprador.
+Educación : El nivel educativo del comprador, como licenciatura, posgrado, escuela secundaria, universidad parcial o escuela secundaria parcial.
+Ocupación : El trabajo u ocupación del comprador, como oficinista, administrativo, manual, profesional o manual especializado.
+Propietario de vivienda : Indica si el comprador es propietario de su casa o no.
+Autos : La cantidad de autos que posee el comprador.
+Distancia de viaje : La distancia entre la casa del comprador y su lugar de trabajo, dividida en cinco categorías : 0 - 1 millas, 1 - 2 millas, 2 - 5 millas, 5 - 10 millas y 10 + millas.
+Región : La región geográfica donde vive el comprador, ya sea en Europa, América del Norte o el Pacífico.
+Edad : La edad del comprador.
+Bicicleta comprada : Indica si el comprador ha comprado una bicicleta o no.
+Variable dependiente Y : Variable que representa si el cliente ha comprado o no la bicicleta.
+Es importante tener en cuenta que la descripción proporcionada se basa en los campos mencionados y su respectivo significado en el contexto del conjunto de datos.
